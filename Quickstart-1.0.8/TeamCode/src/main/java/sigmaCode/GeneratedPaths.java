21f851c86930d21a9c634d79f9e41af5f5a3245d
messier315 <androgries@gmail.com>
2025-02-20 16:54:25 -0500

    skibidi rizz

--- Quickstart-1.0.8/TeamCode/src/main/java/sigmaCode/GeneratedPaths.java
+++ Quickstart-1.0.8/TeamCode/src/main/java/sigmaCode/GeneratedPaths.java
@@ -15,7 +15,7 @@ public class GeneratedPaths {
             .addPath(
                     new BezierLine(
                             new Point(7.200, 65.000, Point.CARTESIAN),
-                            new Point(34.000, 65.000, Point.CARTESIAN)
+                            new Point(32.000, 65.000, Point.CARTESIAN)
                     )
             )
             .setConstantHeadingInterpolation(Math.toRadians(0))
@@ -24,10 +24,10 @@ public class GeneratedPaths {
     public static PathChain line2 = builder
             .addPath(
                     new BezierCurve(
-                            new Point(34.000, 65.000, Point.CARTESIAN),
+                            new Point(32.000, 65.000, Point.CARTESIAN),
                             new Point(1.669, 25.272, Point.CARTESIAN),
-                            new Point(130.172, 25.748, Point.CARTESIAN),
-                            new Point(20.000, 29.801, Point.CARTESIAN)
+                            new Point(115.152, 24.556 , Point.CARTESIAN),
+                            new Point(20.000, 25, Point.CARTESIAN)
                     )
             )
             .setConstantHeadingInterpolation(Math.toRadians(0))
@@ -36,9 +36,9 @@ public class GeneratedPaths {
     public static PathChain line3 = builder
             .addPath(
                     new BezierCurve(
-                            new Point(20.000, 29.801, Point.CARTESIAN),
+                            new Point(14.000, 29.801, Point.CARTESIAN),
                             new Point(112.768, 12.636, Point.CARTESIAN),
-                            new Point(20.000, 15.735, Point.CARTESIAN)
+                            new Point(20.450, 15.735, Point.CARTESIAN)
                     )
             )
             .setConstantHeadingInterpolation(Math.toRadians(0))
@@ -47,10 +47,10 @@ public class GeneratedPaths {
     public static PathChain line4 = builder
             .addPath(
                     new BezierCurve(
-                            new Point(20.000, 15.735, Point.CARTESIAN),
+                            new Point(18.450, 15.735, Point.CARTESIAN),
                             new Point(80.106, 15.974, Point.CARTESIAN),
                             new Point(90.119, 3.815, Point.CARTESIAN),
-                            new Point(20.000, 9.060, Point.CARTESIAN)
+                            new Point(20.404, 8.821, Point.CARTESIAN)
                     )
             )
             .setConstantHeadingInterpolation(Math.toRadians(0))
@@ -58,8 +58,93 @@ public class GeneratedPaths {
 
     public static PathChain line5 = builder
             .addPath(
+                    new BezierCurve(
+                            new Point(20.404, 8.821, Point.CARTESIAN),
+                            new Point(43.152, 26.702, Point.CARTESIAN),
+                            new Point(10.000, 33.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line6 = builder
+            .addPath(
+                    new BezierCurve(
+                            new Point(10.000, 33.000, Point.CARTESIAN),
+                            new Point(34.331, 33.377, Point.CARTESIAN),
+                            new Point(36.000, 65.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line7 = builder
+            .addPath(
                     new BezierLine(
-                            new Point(20.000, 9.060, Point.CARTESIAN),
+                            new Point(36.000, 65.000, Point.CARTESIAN),
+                            new Point(9.000, 32.901, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line8 = builder
+            .addPath(
+                    new BezierCurve(
+                            new Point(9.000, 32.901, Point.CARTESIAN),
+                            new Point(34.331, 33.377, Point.CARTESIAN),
+                            new Point(36.000, 65.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line9 = builder
+            .addPath(
+                    new BezierLine(
+                            new Point(36.000, 65.000, Point.CARTESIAN),
+                            new Point(8.000, 33.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line10 = builder
+            .addPath(
+                    new BezierCurve(
+                            new Point(8.000, 33.000, Point.CARTESIAN),
+                            new Point(34.331, 33.377, Point.CARTESIAN),
+                            new Point(36.000, 65.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line11 = builder
+            .addPath(
+                    new BezierLine(
+                            new Point(36.000, 65.000, Point.CARTESIAN),
+                            new Point(8.000, 33.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line12 = builder
+            .addPath(
+                    new BezierCurve(
+                            new Point(8.000, 33.000, Point.CARTESIAN),
+                            new Point(34.570, 33.616, Point.CARTESIAN),
+                            new Point(36.000, 65.000, Point.CARTESIAN)
+                    )
+            )
+            .setConstantHeadingInterpolation(Math.toRadians(0))
+            .build();
+
+    public static PathChain line13 = builder
+            .addPath(
+                    new BezierLine(
+                            new Point(36.000, 65.000, Point.CARTESIAN),
                             new Point(8.000, 33.000, Point.CARTESIAN)
                     )
             )
